57b895eee368743fb382d27a61fb9df23a523304
Adarshjeet0 <adarshjeetc@gmail.com>
2024-12-16 08:13:07 +0530

    mood predictor app

--- week3/Lec1/03mood-predictor-app/src/App.js
+++ week3/Lec1/03mood-predictor-app/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MOODS = [
+  "happy",
+  "sad",
+  "angry",
+  "excited",
+  "fearful",
+  "disgusted",
+  "surprised"
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      prediction: ""
+    };
+  }
+
+  handleClick() {
+    const moodIndex = Math.floor(Math.random() * MOODS.length);
+    console.log(moodIndex);
+    this.setState({ prediction: MOODS[moodIndex] });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Mood Predictor</h1>
+        {this.state.prediction && (
+          <p>
+            Mood predictor has predicted that you mood will be
+            <b> {this.state.prediction} </b>today.
+          </p>
+        )}
+        <button onClick={this.handleClick.bind(this)}>Predict Mood</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
